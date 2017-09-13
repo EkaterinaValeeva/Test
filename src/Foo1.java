05d0286418f4619be51a9339dcf04cde7ac4eba1
Ekaterina.Valeeva <4quatsch@gmail.com>
2017-09-13 12:37:27 +0700

    Foo

--- src/Foo1.java
+++ src/Foo1.java
@@ -5,7 +5,9 @@ public class Foo1 {
      * @param i an integer to be printed
      * */
     public void printData(int i) {
+
         System.out.println(i);
+        System.out.println(i*2);
     }
 
     public void printData(char a) {
