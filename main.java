d73c4cd05bfee295df9149a5b6ddbf82f0341728
EkaterinaValeeva <Ekaterina.Valeeva@jetbrains.com>
2024-05-17 15:28:43 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public static void main() {
-  system.out.println("Hello the huge world");
+  system.out.println("Hello the beautiful world");
   for (int i = 0; i < 10; i++)
     system.out.println(i);
 }
