6c3caf80fb30f3ee8806f43298f2c65133761f42
Ekaterina.Valeeva <ekaterina.valeeva@jetbrains.com>
2025-01-28 16:23:35 +0100

    test

--- main.java
+++
@@ -1,5 +0,0 @@
-public static void main() {
-  system.out.println("Hello the beautiful world");
-  for (int i = 0; i < 10; i++)
-    system.out.println(i);
-}
---
+++ src/com/main.java
@@ -0,0 +1,9 @@
+package com;
+
+public class main {
+  public static void main () {
+    System.out.println("Hello the beautiful world");
+    for (int i = 0; i < 10; i++)
+      System.out.println(i);
+  }
+}
